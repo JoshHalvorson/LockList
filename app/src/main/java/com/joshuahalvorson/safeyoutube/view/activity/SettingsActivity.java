621e01e4b42adea326e87586d38b84bd1b43b1ca
Josh Halvorson <joshhalvorson16@gmail.com>
2019-04-18 23:27:45 -0700

    Add settings activity and layouts

---
+++ app/src/main/java/com/joshuahalvorson/safeyoutube/view/activity/SettingsActivity.java
@@ -0,0 +1,15 @@
+package com.joshuahalvorson.safeyoutube.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.joshuahalvorson.safeyoutube.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
