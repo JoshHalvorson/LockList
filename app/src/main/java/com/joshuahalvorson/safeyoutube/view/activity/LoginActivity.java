8d68264a40a5936bffe1029680676e80969c2d6e
Josh Halvorson <joshhalvorson16@gmail.com>
2019-04-19 13:04:31 -0700

    Change to SharedPrefs to hold account PW

--- app/src/main/java/com/joshuahalvorson/safeyoutube/view/activity/LoginActivity.java
+++ app/src/main/java/com/joshuahalvorson/safeyoutube/view/activity/LoginActivity.java
@@ -1,10 +1,11 @@
 package com.joshuahalvorson.safeyoutube.view.activity;
 
 import android.arch.persistence.room.Room;
+import android.content.Context;
 import android.content.Intent;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,86 +13,57 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.joshuahalvorson.safeyoutube.R;
-import com.joshuahalvorson.safeyoutube.database.Account;
-import com.joshuahalvorson.safeyoutube.database.AccountDatabase;
 
 public class LoginActivity extends AppCompatActivity {
     private Button loginButton;
     private TextView passwordText;
     private EditText enteredPasswordText;
-    private AccountDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        db = Room.databaseBuilder(getApplicationContext(),
-                AccountDatabase.class, "database-accounts").build();
         loginButton = findViewById(R.id.submit_password_button);
         passwordText = findViewById(R.id.login_text);
         enteredPasswordText = findViewById(R.id.password_edit_text);
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                if(db.accountDao().getAll().size() == 0){
-                    passwordText.setText("Create password");
-                    loginButton.setHint("Create a password");
-                    loginButton.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            new Thread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    db.accountDao().insertAll(
-                                            new Account(enteredPasswordText.getText().toString()));
-                                    runOnUiThread(new Runnable() {
-                                        @Override
-                                        public void run() {
-                                            Intent result = new Intent();
-                                            setResult(RESULT_OK, result);
-                                            finish();
-                                        }
-                                    });
-                                }
-                            }).start();
+        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+        final String password = sharedPref.getString(getString(R.string.account_key), "");
+
+        if (password != null) {
+            if (password.equals("")) {
+                passwordText.setText("Create password");
+                loginButton.setHint("Create a password");
+                loginButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (!enteredPasswordText.getText().toString().equals("")) {
+                            SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+                            SharedPreferences.Editor editor = sharedPref.edit();
+                            editor.putString(getString(R.string.account_key), enteredPasswordText.getText().toString());
+                            editor.apply();
+                            Intent result = new Intent();
+                            setResult(RESULT_OK, result);
+                            finish();
                         }
-                    });
-                }else{
-                    loginButton.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(final View v) {
-                            new Thread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    Account account = db.accountDao().getAccountById(1);
-                                    if(account != null && account.accountPassword.equals(
-                                            enteredPasswordText.getText().toString())){
-                                        runOnUiThread(new Runnable() {
-                                            @Override
-                                            public void run() {
-                                                Intent result = new Intent();
-                                                setResult(RESULT_OK, result);
-                                                finish();
-                                            }
-                                        });
-                                    }else{
-                                        runOnUiThread(new Runnable() {
-                                            @Override
-                                            public void run() {
-                                                Toast.makeText(getApplicationContext(), "Wrong password",
-                                                        Toast.LENGTH_LONG).show();
-                                            }
-                                        });
-                                        
-                                    }
-                                }
-                            }).start();
+                    }
+                });
+            } else {
+                loginButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (password.equals(enteredPasswordText.getText().toString())) {
+                            Intent result = new Intent();
+                            setResult(RESULT_OK, result);
+                            finish();
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Wrong password",
+                                    Toast.LENGTH_LONG).show();
                         }
-                    });
-                }
+                    }
+                });
             }
-        }).start();
+        }
     }
 }
