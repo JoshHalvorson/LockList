ae9e537ff0f1c331bfdb429dfc00b78dc71910e6
JoshHalvorson <joshhalvorson16@gmail.com>
2019-05-15 13:50:42 -0700

    Add query methods to playlist dao

--- app/src/main/java/com/joshuahalvorson/safeyoutube/Kotlin/database/PlaylistDao.kt
+++ app/src/main/java/com/joshuahalvorson/safeyoutube/Kotlin/database/PlaylistDao.kt
@@ -1,9 +1,21 @@
 package com.joshuahalvorson.safeyoutube.Kotlin.database
 
 import android.arch.persistence.room.Dao
+import android.arch.persistence.room.Delete
+import android.arch.persistence.room.Insert
 import android.arch.persistence.room.Query
 
 @Dao
 interface PlaylistDao {
-    
+    @Query("SELECT * FROM playlist")
+    fun getAllPlaylists(): List<Playlist>
+
+    @Query("SELECT * FROM playlist WHERE playlist_id = (:playlistId)")
+    fun getPlaylistById(playlistId: Int): Playlist
+
+    @Insert
+    fun insertAll(vararg playlists: Playlist)
+
+    @Delete
+    fun delete(playlist: Playlist)
 }
