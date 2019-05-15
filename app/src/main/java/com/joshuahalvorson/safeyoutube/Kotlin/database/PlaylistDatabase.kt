6d306dbd4bdc76274c64d0b45ebd308ac1e9af26
JoshHalvorson <joshhalvorson16@gmail.com>
2019-05-15 13:48:22 -0700

    Update playlist database

--- app/src/main/java/com/joshuahalvorson/safeyoutube/Kotlin/database/PlaylistDatabase.kt
+++ app/src/main/java/com/joshuahalvorson/safeyoutube/Kotlin/database/PlaylistDatabase.kt
@@ -1,4 +1,9 @@
 package com.joshuahalvorson.safeyoutube.Kotlin.database
 
-class PlaylistDatabase {
+import android.arch.persistence.room.Database
+import android.arch.persistence.room.RoomDatabase
+
+@Database(entities = [Playlist::class], version = 1)
+abstract class PlaylistDatabase : RoomDatabase() {
+    abstract fun playlistDao(): PlaylistDao
 }
