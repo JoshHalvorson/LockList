f4c23c0bdb250eb7b78401832486543b869fc885
JoshHalvorson <joshhalvorson16@gmail.com>
2019-05-15 13:41:25 -0700

    Add room db dao

---
+++ app/src/main/java/com/joshuahalvorson/safeyoutube/Kotlin/database/PlaylistDao.kt
@@ -0,0 +1,9 @@
+package com.joshuahalvorson.safeyoutube.Kotlin.database
+
+import android.arch.persistence.room.Dao
+import android.arch.persistence.room.Query
+
+@Dao
+interface PlaylistDao {
+    
+}
