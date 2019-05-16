b1c40564d9b283a88d935291a2bcb6122b39a7cb
JoshHalvorson <joshhalvorson16@gmail.com>
2019-05-16 14:31:29 -0700

    Update models

--- app/src/main/java/com/joshuahalvorson/safeyoutube/Kotlin/database/Playlist.kt
+++ app/src/main/java/com/joshuahalvorson/safeyoutube/Kotlin/database/Playlist.kt
@@ -7,7 +7,7 @@ import android.arch.persistence.room.PrimaryKey
 @Entity
 data class Playlist(
         @ColumnInfo(name = "playlist_id") var playlistId: String,
-        @ColumnInfo(name = "playlist_name") var playlistName: String,
+        @ColumnInfo(name = "playlist_name") var playlistName: String?,
         @ColumnInfo(name = "playlist_video_count") var playlistVideoCount: Int,
         @ColumnInfo(name = "playlist_thumbnail") var playlistThumbnail: String){
         @PrimaryKey(autoGenerate = true)
--- app/src/main/java/com/joshuahalvorson/safeyoutube/Kotlin/model/Models.kt
+++ app/src/main/java/com/joshuahalvorson/safeyoutube/Kotlin/model/Models.kt
@@ -1,69 +1,95 @@
 package com.joshuahalvorson.safeyoutube.Kotlin.model
 
 object Models{
-    data class ContentDetails(private var duration: String? = null,
-                              private var dimension: String? = null,
-                              private var definition: String? = null,
-                              private var caption: String? = null,
-                              private var licensedContent: Boolean? = null,
-                              private var projection: String? = null)
+    class ContentDetails{
+        var duration: String? = null
+        var dimension: String? = null
+        var definition: String? = null
+        var caption: String? = null
+        var licensedContent: Boolean? = null
+        var projection: String? = null
+    }
 
-    data class Default (private var url: String? = null,
-                        private var width: Int? = null,
-                        private var height: Int? = null)
+    class Default {
+        var url: String? = null
+        var width: Int? = null
+        var height: Int? = null
+    }
 
-    data class High(private var url: String? = null,
-                    private var width: Int? = null,
-                    private var height: Int? = null)
+    class High{
+        var url: String? = null
+        var width: Int? = null
+        var height: Int? = null
+    }
 
-    data class Item(private var kind: String? = null,
-                    private var etag: String? = null,
-                    private var id: String? = null,
-                    private var snippet: Snippet? = null,
-                    private var contentDetails: ContentDetails? = null)
+    class Item{
+        var kind: String? = null
+        var etag: String? = null
+        var id: String? = null
+        var contentDetails: ContentDetails? = null
+        var snippet: Snippet? = null
+    }
 
-    data class Maxres(private var url: String? = null,
-                      private var width: Int? = null,
-                      private var height: Int? = null)
+    class Maxres{
+        var url: String? = null
+        var width: Int? = null
+        var height: Int? = null
+    }
 
-    data class Medium(private var url: String? = null,
-                      private var width: Int? = null,
-                      private var height: Int? = null)
+    class Medium{
+        var url: String? = null
+        var width: Int? = null
+        var height: Int? = null
+    }
 
-    data class PageInfo(private var totalResults: Int? = null,
-                        private var resultsPerPage: Int? = null)
+    class PageInfo{
+        var totalResults: Int? = null
+        var resultsPerPage: Int? = null
+    }
 
-    data class PlaylistResultOverview(private var kind: String? = null,
-                                      private var etag: String? = null,
-                                      private var nextPageToken: String? = null,
-                                      private var pageInfo: PageInfo? = null,
-                                      private var items: List<Item>? = null)
+    class PlaylistResultOverview{
+        var kind: String? = null
+        var etag: String? = null
+        var nextPageToken: String? = null
+        var pageInfo: PageInfo? = null
+        var items: List<Item> = listOf()
+    }
 
-    data class ResourceId(private var kind: String? = null,
-                          private var videoId: String? = null)
+    class ResourceId{
+        var kind: String? = null
+        var videoId: String? = null
+    }
 
-    data class Snippet(private var publishedAt: String? = null,
-                       private var channelId: String? = null,
-                       private var title: String? = null,
-                       private var description: String? = null,
-                       private var thumbnails: Thumbnails? = null,
-                       private var channelTitle: String? = null,
-                       private var playlistId: String? = null,
-                       private var position: Int? = null,
-                       private var resourceId: ResourceId? = null)
+    class Snippet{
+        var publishedAt: String? = null
+        var channelId: String? = null
+        var title: String? = null
+        var description: String? = null
+        var thumbnails: Thumbnails? = null
+        var channelTitle: String? = null
+        var playlistId: String? = null
+        var position: Int? = null
+        var resourceId: ResourceId? = null
+    }
 
-    data class Standard(private var url: String? = null,
-                        private var width: Int? = null,
-                        private var height: Int? = null)
+    class Standard{
+        var url: String? = null
+        var width: Int? = null
+        var height: Int? = null
+    }
 
-    data class Thumbnails(private var _default: Default? = null,
-                          private var medium: Medium? = null,
-                          private var high: High? = null,
-                          private var standard: Standard? = null,
-                          private var maxres: Maxres? = null)
+    class Thumbnails{
+        var _default: Default? = null
+        var medium: Medium? = null
+        var high: High? = null
+        var standard: Standard? = null
+        var maxres: Maxres? = null
+    }
 
-    data class VideoInfo(private var kind: String? = null,
-                         private var etag: String? = null,
-                         private var pageInfo: PageInfo? = null,
-                         private var items: List<Item>? = null)
+    class VideoInfo{
+        var kind: String? = null
+        var etag: String? = null
+        var pageInfo: PageInfo? = null
+        var items: List<Item>? = null
+    }
 }
