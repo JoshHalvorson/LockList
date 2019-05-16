51c28771f6183f5f356d6514df283eaeb0237639
JoshHalvorson <joshhalvorson16@gmail.com>
2019-05-16 15:15:43 -0700

    Fix recursive calls in viewmodel

--- app/src/main/java/com/joshuahalvorson/safeyoutube/Kotlin/network/YoutubeDataApiViewModel.kt
+++ app/src/main/java/com/joshuahalvorson/safeyoutube/Kotlin/network/YoutubeDataApiViewModel.kt
@@ -11,19 +11,20 @@ class YoutubeDataApiViewModel : ViewModel() {
     private var playlistOverview: MutableLiveData<PlaylistResultOverview>? = null
     private var playlistInfo: MutableLiveData<PlaylistResultOverview>? = null
     private var videoInfo: MutableLiveData<VideoInfo>? = null
+    val repo = YoutubeDataApiRepository()
 
     fun getPlaylistOverview(playlistId: String): MutableLiveData<PlaylistResultOverview>? {
-        playlistOverview = getPlaylistOverview(playlistId)
+        playlistOverview = repo.getPlaylistOverview(playlistId)
         return playlistOverview
     }
 
     fun getPlaylistInfo(playlistId: String): MutableLiveData<PlaylistResultOverview>? {
-        playlistInfo = getPlaylistInfo(playlistId)
+        playlistInfo = repo.getPlaylistInfo(playlistId)
         return playlistInfo
     }
 
     fun getVideoInfo(videoId: String): MutableLiveData<VideoInfo>? {
-        videoInfo = getVideoInfo(videoId)
+        videoInfo = repo.getVideoInfo(videoId)
         return videoInfo
     }
 
