47c4476836514f092a164510ebd2e0382065c7aa
JoshHalvorson <joshhalvorson16@gmail.com>
2019-05-17 17:38:25 -0700

    Add removeplaylist fragment

---
+++ app/src/main/java/com/joshuahalvorson/safeyoutube/Kotlin/view/fragment/RemovePlaylistFragment.kt
@@ -0,0 +1,58 @@
+package com.joshuahalvorson.safeyoutube.Kotlin.view.fragment
+
+import android.arch.persistence.room.Room
+import android.os.Bundle
+import android.support.v4.app.DialogFragment
+import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.joshuahalvorson.safeyoutube.Kotlin.adapter.PlaylistsListRecyclerviewAdapter
+import com.joshuahalvorson.safeyoutube.Kotlin.database.Playlist
+import com.joshuahalvorson.safeyoutube.Kotlin.database.PlaylistDatabase
+
+import com.joshuahalvorson.safeyoutube.R
+import kotlinx.android.synthetic.main.fragment_remove_playlist_dialog.*
+
+class RemovePlaylistFragment : Fragment() {
+    private lateinit var adapter: PlaylistsListRecyclerviewAdapter
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_remove_playlist_dialog, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        var playlists = ArrayList<Playlist>()
+        val db = Room.databaseBuilder<PlaylistDatabase>(context!!,
+                PlaylistDatabase::class.java, "database-playlists").build();
+
+        adapter = PlaylistsListRecyclerviewAdapter(true, playlists, object : PlaylistsListRecyclerviewAdapter.OnListItemClick {
+            override fun onListItemClick(playlist: Playlist?) {
+                if (playlist != null) {
+                    Thread(Runnable {
+                        db.playlistDao().delete(playlist)
+                        playlists.remove(playlist)
+                        activity?.runOnUiThread {
+                            adapter.notifyDataSetChanged()
+                            db.close()
+                        }
+                    }).start()
+                }
+            }
+        })
+
+        playlists_to_remove_list.layoutManager = LinearLayoutManager(context)
+        playlists_to_remove_list.adapter = adapter
+
+        Thread(Runnable {
+            val tempPlaylists = db.playlistDao().getAllPlaylists()
+            activity?.runOnUiThread{
+                playlists.clear()
+                playlists.addAll(tempPlaylists)
+                adapter.notifyDataSetChanged()
+            }
+        }).start()
+    }
+}
