4037fd83c992d8104f4b2dffdda77f46cb96c72f
JoshHalvorson <joshhalvorson16@gmail.com>
2019-05-20 16:06:21 -0700

    Add current logged in google accounts youtube playlists to db
    Currently, playing videos from playlist doesnt work, presumable because the api calls im making dont check if user is authenticated.

--- app/src/main/java/com/joshuahalvorson/safeyoutube/Kotlin/view/fragment/PlaylistsListFragment.kt
+++ app/src/main/java/com/joshuahalvorson/safeyoutube/Kotlin/view/fragment/PlaylistsListFragment.kt
@@ -14,13 +14,27 @@ import com.joshuahalvorson.safeyoutube.Kotlin.database.Playlist
 import com.joshuahalvorson.safeyoutube.Kotlin.database.PlaylistDatabase
 import com.joshuahalvorson.safeyoutube.R
 import kotlinx.android.synthetic.main.fragment_playlists_list.*
-import java.util.ArrayList
 import android.content.DialogInterface
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential
+import com.google.api.client.util.ExponentialBackOff
+import com.google.api.services.youtube.YouTubeScopes
+import java.util.*
+import com.google.api.client.googleapis.extensions.android.gms.auth.UserRecoverableAuthIOException
+import com.google.api.client.googleapis.extensions.android.gms.auth.GooglePlayServicesAvailabilityIOException
+import com.google.api.client.json.jackson2.JacksonFactory
+import com.google.api.client.extensions.android.http.AndroidHttp
+import android.os.AsyncTask
+import android.widget.Toast
+import com.google.android.gms.common.GoogleApiAvailability
+import com.google.api.services.youtube.YouTube
+import kotlinx.io.IOException
+import kotlin.collections.ArrayList
 
 class PlaylistsListFragment : Fragment() {
     private var playlists: ArrayList<Playlist> = ArrayList()
     private lateinit var adapter: PlaylistsListRecyclerviewAdapter
     var db: PlaylistDatabase? = null
+    lateinit var googleAccountCredential: GoogleAccountCredential
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -65,6 +79,18 @@ class PlaylistsListFragment : Fragment() {
                 }
             }
         }
+
+        googleAccountCredential = GoogleAccountCredential.usingOAuth2(
+                context, Arrays.asList(*arrayOf(YouTubeScopes.YOUTUBE_READONLY)))
+                .setBackOff(ExponentialBackOff())
+        val accountName = activity?.getPreferences(Context.MODE_PRIVATE)
+                ?.getString("account_name", null);
+        if (accountName != null) {
+            googleAccountCredential.selectedAccountName = accountName;
+            MakeRequestTask().execute();
+        }else{
+            //not logged in
+        }
     }
 
     private fun startAddPlaylistFragment(url: String?, showFrag: Boolean) {
@@ -108,4 +134,96 @@ class PlaylistsListFragment : Fragment() {
         super.onDestroy()
         db?.close()
     }
+
+    private inner class MakeRequestTask : AsyncTask<Void, Void, ArrayList<Playlist>>() {
+        private var youtubeService: YouTube? = null
+        private var lastError: Exception? = null
+
+        override fun doInBackground(vararg params: Void): ArrayList<Playlist>? {
+            try {
+                return getDataFromApi()
+            } catch (e: Exception) {
+                lastError = e
+                cancel(true)
+                return null
+            }
+
+        }
+
+        @Throws(IOException::class)
+        private fun getDataFromApi(): ArrayList<Playlist> {
+            val transport = AndroidHttp.newCompatibleTransport()
+            val jsonFactory = JacksonFactory.getDefaultInstance()
+            youtubeService = YouTube.Builder(
+                    transport, jsonFactory, googleAccountCredential)
+                    .setApplicationName("SafeYoutube")
+                    .build()
+
+            val tempList = ArrayList<Playlist>()
+
+            val result = youtubeService?.playlists()?.list("snippet, contentDetails")
+                    ?.setMine(true)
+                    ?.execute()
+
+            result?.items?.forEach {
+                if(db?.playlistDao()?.getPlaylistById(it.id)!!){
+                    //playlist already in db
+                    return tempList
+                }else{
+                    val playlist = Playlist(it.id,
+                            it.snippet.title,
+                            it.contentDetails.itemCount.toInt(),
+                            it.snippet.thumbnails.default.url)
+
+                    db?.playlistDao()?.insertAll(playlist)
+                    tempList.add(playlist)
+                }
+            }
+            return tempList
+        }
+
+        override fun onPreExecute() {
+            //mProgress.show()
+        }
+
+        override fun onPostExecute(output: ArrayList<Playlist>?) {
+            //mProgress.hide()
+            if (output == null || output.size == 0) {
+                //account playlists already added
+            } else {
+                playlists.addAll(output)
+                adapter.notifyDataSetChanged()
+            }
+        }
+
+        override fun onCancelled() {
+            //mProgress.hide()
+            if (lastError != null) {
+                if (lastError is GooglePlayServicesAvailabilityIOException) {
+                    showGooglePlayServicesAvailabilityErrorDialog(
+                            (lastError as GooglePlayServicesAvailabilityIOException)
+                                    .connectionStatusCode)
+                } else if (lastError is UserRecoverableAuthIOException) {
+                    startActivityForResult(
+                            (lastError as UserRecoverableAuthIOException).intent,
+                            1003)
+                } else {
+                    Toast.makeText(context, "The following error occurred:\n" + lastError!!.message, Toast.LENGTH_LONG).show()
+                }
+            } else {
+                Toast.makeText(context, "Request cancelled.", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    internal fun showGooglePlayServicesAvailabilityErrorDialog(
+            connectionStatusCode: Int) {
+        val apiAvailability = GoogleApiAvailability.getInstance()
+        val dialog = apiAvailability.getErrorDialog(
+                activity,
+                connectionStatusCode,
+                1002)
+        dialog.show()
+    }
+
 }
