c48d9cd539e7a382e98e815da2e22693bd64671d
JoshHalvorson <joshhalvorson16@gmail.com>
2019-05-22 10:58:25 -0700

    Move packages

--- app/src/main/java/com/joshuahalvorson/safeyoutube/kotlin/database/PlaylistDatabase.kt
+++
@@ -1,9 +0,0 @@
-package com.joshuahalvorson.safeyoutube.kotlin.database
-
-import androidx.room.Database
-import androidx.room.RoomDatabase
-
-@Database(entities = [Playlist::class], version = 1)
-abstract class PlaylistDatabase : RoomDatabase() {
-    abstract fun playlistDao(): PlaylistDao
-}
